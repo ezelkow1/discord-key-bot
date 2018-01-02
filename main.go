1711bc8c7617b35f680f540072a6d443a960459d
Evan Zelkowitz <19699200+ezelkow1@users.noreply.github.com>
2018-01-02 16:39:37 -0700

    saving this off. Can check for !add, also split on comma and output username. Will need to do a kv store using game name as key and put serials in a variable array

--- main.go
+++ main.go
@@ -5,9 +5,11 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"strings"
 	"syscall"
 
 	"github.com/bwmarrin/discordgo"
+	"github.com/rapidloop/skv"
 )
 
 // Variables used for command line parameters
@@ -30,6 +32,12 @@ func main() {
 		return
 	}
 
+	store, err := skv.Open("keys.db")
+	store.Put("teststuff", "123")
+	store.Put("teststuff", "456")
+	var info string
+	store.Get("teststuff", &info)
+	fmt.Println("testinfo: ", info)
 	// Register the messageCreate func as a callback for MessageCreate events.
 	dg.AddHandler(messageCreate)
 
@@ -53,15 +61,18 @@ func main() {
 // This function will be called (due to AddHandler above) every time a new
 // message is created on any channel that the autenticated bot has access to.
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
-
+	s.ChannelMessage(m.ChannelID, "weeeeee")
 	// Ignore all messages created by the bot itself
 	// This isn't required in this specific example but it's a good practice.
 	if m.Author.ID == s.State.User.ID {
 		return
 	}
 	// If the message is "ping" reply with "Pong!"
-	if m.Content == "ping" {
-		s.ChannelMessageSend(m.ChannelID, "Pong!")
+	if strings.HasPrefix(m.Content, "!add ") == true {
+		output := strings.Split(m.Content, ",")
+		fmt.Println("output ", output[1])
+		mout := []string{"Adding ", output[1], " from user ", m.Author.Username}
+		s.ChannelMessageSend(m.ChannelID, strings.Join(mout, " "))
 	}
 
 	// If the message is "pong" reply with "Ping!"
