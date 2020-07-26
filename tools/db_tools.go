1875a05543ffc86adb10cd9d68f83eaee32e6146
Evan Zelkowitz <evan.zelkowitz@gmail.com>
2020-07-25 19:32:57 -0700

    Add support for database conversion to sqlite format Add printing and deletion tool support for the new format as well

--- tools/db_tools.go
+++ tools/db_tools.go
@@ -1,11 +1,16 @@
 package main
 
 import (
+	"database/sql"
 	"encoding/json"
 	"flag"
 	"fmt"
+	"log"
 	"os"
 	"strconv"
+	"strings"
+
+	_ "github.com/mattn/go-sqlite3"
 )
 
 //GameKey struct
@@ -18,18 +23,27 @@ type GameKey struct {
 
 var (
 	// Game key Database
-	x          = make(map[string][]GameKey)
-	author     string
-	printout   bool
-	deleteauth bool
-	dbFile     = "keys.db"
+	x               = make(map[string][]GameKey)
+	author          string
+	oldprintout     bool
+	olddeleteauth   bool
+	printout        bool
+	deleteauth      bool
+	convert         bool
+	dbFile          string
+	converteddbFile string
+	//database        *sql.DB
 )
 
 func init() {
-
-	flag.BoolVar(&printout, "p", false, "print keys for author")
-	flag.BoolVar(&deleteauth, "d", false, "delete keys from author")
+	flag.BoolVar(&oldprintout, "op", false, "print keys for author (Old style DB)")
+	flag.BoolVar(&olddeleteauth, "od", false, "delete keys from author (Old style DB)")
 	flag.StringVar(&author, "author", "", "-author=authorname")
+	flag.BoolVar(&convert, "c", false, "Convert an old style database to the newer sql format")
+	flag.StringVar(&dbFile, "olddb", "keys.db", "Optional name of old db (need to set if not default). -olddb=originalDBfile.db")
+	flag.StringVar(&converteddbFile, "newdb", "keys-sqlite.db", "Optional name of the new sql formatted database output. -newdb=keys-sqlite.db")
+	flag.BoolVar(&printout, "p", false, "print keys for author (New style DB)")
+	flag.BoolVar(&deleteauth, "d", false, "delete keys from author (New style DB)")
 	flag.Parse()
 
 }
@@ -37,19 +51,264 @@ func init() {
 func main() {
 
 	if author != "" {
-		if printout {
+		if oldprintout {
 			// print out authors keys
+			oldprintKeys()
+		}
+
+		if olddeleteauth {
+			// delete authors keys
+			olddeleteAuth()
+		}
+
+		if printout {
 			printKeys()
 		}
 
 		if deleteauth {
-			// delete authors keys
 			deleteAuth()
 		}
+	}
+
+	if convert {
+		convertDB()
 	}
 }
 
 func printKeys() {
+	database, err := sql.Open("sqlite3", converteddbFile)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer database.Close()
+
+	statement, err := database.Query("SELECT key, users.name as user_name, games.pretty as game FROM keys INNER JOIN users on users.id = keys.userid_entered INNER JOIN games on games.id = keys.game_id WHERE user_name = (?)", author)
+
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	for statement.Next() {
+		var key string
+		var author string
+		var game string
+		statement.Scan(&key, &author, &game)
+		println("\nGame: " + game + ", key: " + key + ", Author: " + author)
+	}
+	statement.Close()
+}
+
+func deleteAuth() {
+	database, err := sql.Open("sqlite3", converteddbFile)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer database.Close()
+	var userid int
+	deleterow, err := database.Prepare("DELETE from keys where userid_entered = (?)")
+	defer deleterow.Close()
+	statement, err := database.Query("SELECT id from users where name = (?)", author)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	for statement.Next() {
+		statement.Scan(&userid)
+	}
+	println("Deleting all keys from " + author + ":" + strconv.Itoa(userid))
+	deleterow.Exec(userid)
+}
+
+func insertGames() {
+	Load(dbFile, &x)
+	if len(x) <= 0 {
+		println("db: " + dbFile + " is empty")
+		return
+	}
+	database, err :=
+		sql.Open("sqlite3", converteddbFile)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer database.Close()
+	statement, err := database.Prepare("INSERT INTO games (normalized, pretty) VALUES (?,?)")
+	if err != nil {
+		panic(err)
+	}
+
+	var count int
+	for key := range x {
+		for game := range x[key] {
+			_, err = statement.Exec(key, x[key][game].GameName)
+			if err == nil {
+				count++
+			}
+			continue
+		}
+	}
+
+	println("Inserted " + strconv.Itoa(count) + " distinct games in to the database")
+	statement.Close()
+}
+
+func insertUsers() {
+	Load(dbFile, &x)
+	if len(x) <= 0 {
+		println("db: " + dbFile + " is empty")
+		return
+	}
+	database, err :=
+		sql.Open("sqlite3", converteddbFile)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer database.Close()
+	statement, err := database.Prepare("INSERT INTO users (name) VALUES (?)")
+	if err != nil {
+		panic(err)
+	}
+
+	var count int
+	for key := range x {
+		for game := range x[key] {
+			_, err = statement.Exec(x[key][game].Author)
+
+			if err == nil {
+				count++
+			}
+		}
+	}
+	statement.Close()
+	println("Inserted " + strconv.Itoa(count) + " distinct users in to the database (this will include nickname changes users may have made while entering keys)")
+}
+
+func insertKeys() {
+	Load(dbFile, &x)
+	if len(x) <= 0 {
+		println("db: " + dbFile + " is empty")
+		return
+	}
+	database, err :=
+		sql.Open("sqlite3", converteddbFile)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	defer database.Close()
+	insert, err := database.Prepare("INSERT INTO keys (key, game_id, userid_entered, service) VALUES (?,?,?,?)")
+	defer insert.Close()
+	if err != nil {
+		panic(err)
+	}
+
+	var count int
+	var foundErr bool
+	for key := range x {
+		for game := range x[key] {
+			findgame, err := database.Query("SELECT id from games where normalized=(?)", NormalizeGame(x[key][game].GameName))
+			var gameid int
+			var userid int
+			for findgame.Next() {
+				err = findgame.Scan(&gameid)
+				if err != nil {
+					panic(err)
+				}
+			}
+			findgame.Close()
+			finduser, err := database.Query("SELECT id from users where name=(?)", x[key][game].Author)
+			for finduser.Next() {
+				err = finduser.Scan(&userid)
+				if err != nil {
+					panic(err)
+				}
+			}
+			finduser.Close()
+			_, err = insert.Exec(x[key][game].Serial, gameid, userid, x[key][game].ServiceType)
+			if err != nil {
+				log.Print(err)
+				println("   For Game: " + x[key][game].GameName + " , Serial: " + x[key][game].Serial + ", Author: " + x[key][game].Author)
+				foundErr = true
+				dupe, err := database.Query("SELECT key, users.name as user_name, games.pretty as game FROM keys INNER JOIN users on users.id = keys.userid_entered INNER JOIN games on games.id = keys.game_id WHERE key = (?)", x[key][game].Serial)
+				if err != nil {
+					log.Print(err)
+				}
+				for dupe.Next() {
+					var key string
+					var gameid string
+					var userid string
+					dupe.Scan(&key, &userid, &gameid)
+					println("   Duplicate Game in DB: " + gameid + ", key: " + key + ", Author: " + userid)
+				}
+				dupe.Close()
+			} else {
+				count++
+			}
+		}
+	}
+
+	if foundErr {
+		println("\n\nIf this is a unique failure this game key already matches an existing one so you have a bad entry somewhere.\nThis is only a notification, the duplicate game is already in the database and this duplicate will not be entered\n\n")
+	}
+	println("Inserted " + strconv.Itoa(count) + " game keys in to the database")
+}
+
+func convertDB() {
+
+	Load(dbFile, &x)
+	if len(x) <= 0 {
+		println("db: " + dbFile + " is empty")
+		return
+	}
+
+	database, err :=
+		sql.Open("sqlite3", converteddbFile)
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	defer database.Close()
+	statement, err := database.Prepare("CREATE TABLE IF NOT EXISTS games (id INTEGER PRIMARY KEY, normalized TEXT NOT NULL, pretty TEXT NOT NULL, UNIQUE(normalized, pretty))")
+	_, err = statement.Exec()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+
+	statement, _ = database.Prepare("CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT NOT NULL UNIQUE, discordid INTEGER UNIQUE)")
+	_, err = statement.Exec()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	statement, err =
+		database.Prepare("CREATE TABLE IF NOT EXISTS" +
+			" keys (id INTEGER PRIMARY KEY, key TEXT NOT NULL, game_id INTEGER NOT NULL, userid_entered INTEGER NOT NULL, userid_taken INTEGER, service TEXT," +
+			"UNIQUE(key) " +
+			"FOREIGN KEY (game_id) REFERENCES games(id) " +
+			"FOREIGN KEY (userid_entered) REFERENCES users(id) " +
+			"FOREIGN KEY (userid_taken) REFERENCES users(id))")
+
+	_, err = statement.Exec()
+	if err != nil {
+		log.Fatal(err)
+		return
+	}
+	statement.Close()
+
+	println("New database " + converteddbFile + " initialized, converting " + dbFile)
+
+	insertGames()
+	insertUsers()
+	insertKeys()
+
+}
+
+func oldprintKeys() {
 
 	Load(dbFile, &x)
 	if len(x) <= 0 {
@@ -68,7 +327,7 @@ func printKeys() {
 	}
 }
 
-func deleteAuth() {
+func olddeleteAuth() {
 	println("In delete auth")
 	Load(dbFile, &x)
 	if len(x) <= 0 {
@@ -134,4 +393,11 @@ func Load(path string, object interface{}) {
 	json.Unmarshal(b, &object)
 	fileh.Close()
 	return
+}
+
+//NormalizeGame the name of the game, removes spaces, lowercases
+func NormalizeGame(name string) string {
+	tmp := strings.ToLower(name)
+	tmp = strings.Replace(tmp, " ", "", -1)
+	return tmp
 }
